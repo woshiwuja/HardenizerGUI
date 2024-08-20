edc57c6855a220e9c92077c096e2f6ae8a1dfd3a
Federico Gabusi <federico.gabusi@marchesini.com>
2024-08-20 08:31:09 +0200

    Up

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pandas as pd
 import excel_parser as xp
 from markupsafe import escape
 app = Flask(__name__)
-
+df_reg = pd.read_xml("assets/registries.xml")
+df_cmd = pd.read_xml("assets/commands.xml")
 
 @app.route("/")
 def index():
@@ -30,27 +31,20 @@ def registry(id=None):
 @app.route("/commands")
 @app.route("/commands/<id>")
 def commands(id=None):
-    df = pd.read_xml("assets/commands.xml")
     if id == None:
-        return render_template('commands.html', commands_dataframe = df.iloc[0])
+        return render_template('commands.html', commands_dataframe = df_cmd.iloc[0])
     else:
-        df = df.loc[df['id'] == int(id)]
+        df = df_cmd.loc[df_cmd['id'] == int(id)]
         return render_template("commands.html",commands_dataframe = df )
 @app.route("/sidebar")
 def sidebar():
     #choice = request.args.get('choice','')
     #if choice == 'registry':
     if request.args.get('sidebar') == "registries": 
-        df = pd.read_xml("assets/registries.xml")
-        registry_dataframe = xp.parse_registry_dataframe(df)
-        return render_template('sidebar.html',df = registry_dataframe, path="/registry")
+        return render_template('sidebar.html',df = df_reg, path="/registry")
     elif request.args.get('sidebar') == "commands":
-        df = pd.read_xml("assets/commands.xml")
-        command_dataframe = df
-        return render_template('sidebar.html',df = command_dataframe, path="/commands")
+        return render_template('sidebar.html',df = df_cmd, path="/commands")
     else:
-        df = pd.read_xml("assets/registries.xml")
-        registry_dataframe = xp.parse_registry_dataframe(df)
-        return render_template('sidebar.html',df = registry_dataframe, path="/registry")
+        return render_template('sidebar.html',df = df_reg, path="/registry")
 if __name__ == '__main__':
     app.run()
